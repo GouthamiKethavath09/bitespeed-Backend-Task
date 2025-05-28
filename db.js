554756dd71e7d450f567f3da8290666156838f20
GouthamiKethavath09 <120586764+GouthamiKethavath09@users.noreply.github.com>
2025-05-28 17:59:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
-import { readFileSync } from 'fs';
+const fs = require('fs');
 
 const db = new sqlite3.Database('./database.db');
 
 function init() {
-    const schema = readFileSync('./schema.sql', 'utf8');
+    const schema = fs.readFileSync('./schema.sql', 'utf8');
     db.exec(schema, (err) => {
         if (err) console.error('Schema Error:', err);
         else console.log('Database initialized.');
     });
 }
 
-export default { db, init };
+module.exports = { db, init };
